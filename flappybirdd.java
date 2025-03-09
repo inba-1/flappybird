0348cbb15b45c6828f0d691db1a608d0ad1bd2de
Inba V <143822939+inba-1@users.noreply.github.com>
2025-03-09 22:29:50 +0530

    uppipe loogic is created

--- flappybirdd.java
+++ flappybirdd.java
@@ -153,8 +153,8 @@ int openspace=boardHeight/4;
     public boolean collision(Bird a , pipe b){
         return a.x < b.x + b.width &&//a's top left corner doesn't reach b's top right corner
         a.x  + a.width > b.x &&//a's top right corner passes b's top left corner
-        a.y < b.y+ b.width && //a's top left corner doesn't reach b's bottom left corner
-        a.y  + a.width > b.y; //a's bottom left corner passes b's top left corner
+        a.y < b.y+ b.height && //a's top left corner doesn't reach b's bottom left corner
+        a.y  + a.height > b.y; //a's bottom left corner passes b's top left corner
         
 
 
